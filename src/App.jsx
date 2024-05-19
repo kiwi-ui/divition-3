0cdc4db071f7436456aab550406c1006e3482900
kiwi-ui <yusnianggara@gmail.com>
2024-05-19 08:56:15 +0700

    halo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Couple from './pages/Couple'
 import Opening from './pages/Opening/Opening'
-import Stories from './pages/Stories'
-import Content1 from './pages/Stories/Content1'
-import Content2 from './pages/Stories/Content2'
-import Content3 from './pages/Stories/Content3'
 
 export const MyContext = createContext();
 
@@ -25,39 +21,12 @@ function App() {
     empat: false,
     lima: false,
   })
-  const [hasInteracted, setHasInteracted] = useState(false);
-  useEffect(() => {
-    // Only trigger the timer if the user has interacted with the stories
-    if (hasInteracted) {
-      const timer = setTimeout(() => {
-        // Logic to transition to next story content
-        if (stories.satu) {
-          setStories(prevState => ({ ...prevState, satu: false, dua: true }));
-          setSeen(prevState => ({...prevState, satu: true}));
-        } else if (stories.dua) {
-          setStories(prevState => ({ ...prevState, dua: false, tiga: true }));
-          setSeen(prevState => ({...prevState, dua: true}));
-        } else if (stories.tiga) {
-          setStories(prevState => ({ ...prevState, tiga: false }));
-          setSeen(prevState => ({...prevState, tiga: true}))
-        } else {
-          // Close the story after showing the third content
-          setStories(({satu: false, dua: false, tiga: false }));
-        }
-      }, 4000); // Transition after 6 seconds
 
-      return () => clearTimeout(timer); // Cleanup timer on unmount
-    }
-  }, [stories, hasInteracted]);
   return (
-      <MyContext.Provider value={{stories, setStories, seen, setSeen, hasInteracted, setHasInteracted}}>
-            <Opening />
-            <Couple />
-            <Stories />
-            {stories.satu ? <Content1 /> : null}
-          {stories.dua ? <Content2 /> : null}
-          {stories.tiga ? <Content3 /> : null}
-            <Navbar />        
+      <MyContext.Provider value={{ stories, setStories, seen, setSeen }}>
+        <Opening />
+        <Couple />
+        <Navbar />        
       </MyContext.Provider>
   )
 }
--- src/pages/Couple/index.jsx
+++ src/pages/Couple/index.jsx
@@ -9,17 +9,18 @@ const Couple = () => {
         <div className={`${style.bgColorPrimary} text-center py-5`} id="main">
             <div className="container">
                 <div className="text-black">
-                    <p className="fw-semibold fs-2">The Wedding Of</p>
-                    <p className="lead mx-5">Dengan rahmat ridho Allah Subhanahu Wa Taala, kami menyelenggarakan acara resepsi pernikahan putra-putri kami:</p>
+                    <p className="fw-semibold fs-1 mb-2">Y&A</p>
+                    <p className="fs-3">Ar-Rum 21</p>
+                    <p className="lead mx-5">Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan sayang. Sungguh, pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir</p>
                 </div>
 
                 <div className="text-black d-flex flex-lg-row flex-column justify-content-around my-4 gap-4 mx-2">
-                    <div className={`bride d-flex flex-column align-items-center ${style.bgTransparent1} py-4 rounded-4 shadow-lg `}>
+                    <div className={`bride d-flex flex-column align-items-center ${style.bgTransparent1} shadow-lg `}>
                         <div className="rounded-circle w-75 position-relative" style={{zIndex: 2}}>
-                            <img src={ bride } className={`rounded-circle shadow `} width="100%" alt="bride"/>
-                            <div className={`rounded-circle shadow p-2 position-relative bg-transparent position-absolute h-100 w-100 border border-white border-2`} style={{left: '0.5rem', top:'0.5rem'}} width="100%" alt="bride"></div>
+                            <img src={ bride } className={`shadow `} alt="bride"/>
+                            <div className={`rounded-circle shadow p-2 position-relative bg-transparent position-absolute h-100 w-100 `} style={{left: '0.5rem', top:'0.5rem'}} width="100%" alt="bride"></div>
                         </div>
-                        <div className={`${style.details} shadow position-relative rounded-3 py-3 px-2 ${style.z10}`}>
+                        {/* <div className={`${style.details} shadow position-relative rounded-3 py-3 px-2 ${style.z10}`}>
                             <img className="position-absolute h-50 translate-middle-x opacity-50" src={flower} alt="flower"/>
                             <p className={`fs-1 fw-bold mb-0 ${style.pengantin}`}>Yusni</p>
                             <p className="fs-5 ">Putri dari Bapak Achmad dan Ibu Rum</p>
@@ -28,23 +29,13 @@ const Couple = () => {
                                     <BsInstagram className='text-black fs-4'/>
                                 </a>
                             </button>
-                        </div>
+                        </div> */}
                     </div>
                     
                     <div className={`groom d-flex flex-column align-items-center ${style.bgTransparent1} py-4 rounded-4 shadow-lg `}>
                         <div className="rounded-circle w-75 position-relative" style={{zIndex: 2}}>
                             <img src={ groom } className={`rounded-circle shadow`} width="100%" alt="groom" />
                             <div className={`rounded-circle shadow p-2 position-relative bg-transparent position-absolute h-100 w-100 border border-white border-2`} style={{left: '0.5rem', top:'0.5rem'}} width="100%" alt="bride"></div>
-                        </div>
-                        <div className={`${style.details} shadow position-relative rounded-3 py-3 px-2 ${style.z10}`}>
-                            <img className="position-absolute h-50 translate-middle-x opacity-50" src={flower} alt="flower"/>
-                            <p className={`fs-1 fw-bold mb-0 ${style.pengantin}`}>Anggara</p>
-                            <p className="fs-5">Putri dari Bapak Achmad dan Ibu Rum</p>
-                            <button className="border-0 rounded-circle bg-transparent">
-                                <a href="https://www.instagram.com/yusnianggara/" target='_blank' rel="noreferrer">
-                                    <BsInstagram className='text-black fs-4'/>
-                                </a>
-                            </button>
                         </div>
                     </div>
                 </div>
